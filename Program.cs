beae889c251d50d4995470531950735d9a647f54
vicdem989 <vicdemhoy@hotmail.com>
2024-04-23 21:31:31 +0200

    Testing ArrayList :D

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Diagnostics;
 using System.Threading;
 
@@ -7,10 +7,29 @@ namespace MOOOV
 {
     public class Program
     {
+
+       public static ArrayList testing = new ArrayList();
+        
+
+
+
         public static void Main()
         {
+
+            testing.Add("hei");
+            foreach(string test in testing)
+            {
+                Console.WriteLine(test);
+            }
+            
+
+        }
+
+            /*
             string[] colors = { "black", "yellow", "red", "blue", "green" };
             string randomColor;
+
+            
 
             List<string> sequence = new List<string>();
 
@@ -34,6 +53,6 @@ namespace MOOOV
             } while (sequence.Count > 0 && sequence[sequence.Count - 1] == randomColor);
 
             return randomColor;
-        }
+        }*/
     }
 }
